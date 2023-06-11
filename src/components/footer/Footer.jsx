adf2654c2432a980007a473cdfcdd1e261a6ba12
Muhammad Ahsan Farooq <ahsanfarooq210@gmail.com>
2023-06-11 12:23:07 -0700

    footer section done
    Link
    
    https://www.youtube.com/watch?v=dFgzHOX84xQ&t=3723s
    
    duration
    
    1:10:59 / 1:35:38
    
    •
    Hamburger Menu HTML

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,43 +9,86 @@ import iconInstagram from "../../assets/icon-instagram.svg";
 const Footer = () => {
   return (
     <footer className="bg-veryDarkBlue">
-      <div className="container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md: flex-row md:space-y-0 ">
+      <div className="container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-row md:space-y-0">
         {/* Logo and social links container */}
         <div className="flex flex-col-reverse items-center justify-between space-y-12 md:flex-col md:space-y-0 md:items-start">
-          {/* logo   */}
-          <div>
-            <img src={logo} alt="logo" className="h-8" />
+          <div className="mx-auto my-6 text-center text-white md:hidden">
+            Copyright © 2022, All Rights Reserved
           </div>
-          {/* social links container */}
-          <div className="flex justify-center space-x-4 ">
-            {/* link 1 */}
+          {/* Logo */}
+          <div>
+            <img src={logo} className="h-8" alt />
+          </div>
+          {/* Social Links Container */}
+          <div className="flex justify-center space-x-4">
+            {/* Link 1 */}
             <a href="#">
-              <img src={iconFacebook} className="h-8" alt="" />
+              <img src={iconFacebook} alt className="h-8" />
             </a>
-
-            {/* link 2 */}
+            {/* Link 2 */}
             <a href="#">
-              <img src={iconYoutube} className="h-8" alt="" />
+              <img src={iconYoutube} alt className="h-8" />
             </a>
-
-            {/* link 3 */}
+            {/* Link 3 */}
             <a href="#">
-              <img src={iconTwitter} className="h-8" alt="" />
+              <img src={iconTwitter} alt className="h-8" />
             </a>
-
-            {/* link 4 */}
+            {/* Link 4 */}
             <a href="#">
-              <img src={iconPintrest} className="h-8" alt="" />
+              <img src={iconPintrest} alt className="h-8" />
             </a>
-
-            {/* link 5 */}
+            {/* Link 5 */}
             <a href="#">
-              <img src={iconInstagram} className="h-8" alt="" />
+              <img src={iconInstagram} alt className="h-8" />
             </a>
           </div>
         </div>
-        {/* list container  */}
-        <div></div>
+        {/* List Container */}
+        <div className="flex justify-around space-x-32">
+          <div className="flex flex-col space-y-3 text-white">
+            <a href="#" className="hover:text-brightRed">
+              Home
+            </a>
+            <a href="#" className="hover:text-brightRed">
+              Pricing
+            </a>
+            <a href="#" className="hover:text-brightRed">
+              Products
+            </a>
+            <a href="#" className="hover:text-brightRed">
+              About
+            </a>
+          </div>
+          <div className="flex flex-col space-y-3 text-white">
+            <a href="#" className="hover:text-brightRed">
+              Careers
+            </a>
+            <a href="#" className="hover:text-brightRed">
+              Community
+            </a>
+            <a href="#" className="hover:text-brightRed">
+              Privacy Policy
+            </a>
+          </div>
+        </div>
+        {/* Input Container */}
+        <div className="flex flex-col justify-between">
+          <form>
+            <div className="flex space-x-3">
+              <input
+                type="text"
+                className="flex-1 px-4 rounded-full focus:outline-none"
+                placeholder="Updated in your inbox"
+              />
+              <button className="px-6 py-2 text-white rounded-full bg-brightRed hover:bg-brightRedLight focus:outline-none">
+                Go
+              </button>
+            </div>
+          </form>
+          <div className="hidden text-white md:block">
+            Copyright © 2022, All Rights Reserved
+          </div>
+        </div>
       </div>
     </footer>
   );
